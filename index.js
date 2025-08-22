159d92c662aa6f2bdc2bbefbd804cc827e15a5b9
Kafi <ahkafi05@gmail.com>
2025-08-22 06:11:13 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+    res.send("server is running!")
+})
+
+app.listen(port, ()=>{
+    console.log(`server running on port ${port}`)
+})
+
