d1c9f07e838142d29b5666debcb48d3856d0a34f
Kafi <ahkafi05@gmail.com>
2025-08-22 17:08:13 +0600

    get all data, get data by id api call added

--- index.js
+++ index.js
@@ -14,6 +14,49 @@ app.get('/', (req, res)=>{
     res.send("server is running!")
 })
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b2jcn5g.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("✅ You successfully connected to MongoDB!");
+
+    const productCollection = client.db('productsDB').collection('products')
+
+    app.get('/products', async (req, res)=>{
+        const result = await productCollection.find().toArray()
+        res.send(result)
+    })
+
+    app.get('/products/:id', async (req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)}
+
+      const result = await productCollection.findOne(query)
+      res.send(result)
+    })
+
+    app.post('/add-product', async(req, res)=>{
+        const data = req.body;
+         const result = await productCollection.insertOne(data);
+         res.status(201).send(result);
+    })
+
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port, ()=>{
     console.log(`server running on port ${port}`)
 })
